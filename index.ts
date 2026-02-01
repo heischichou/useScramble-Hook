2619c5553cb13b646f890b6b75830cdb7042aacb
Jan Michael Garot <jangarot@gmail.com>
2026-02-01 20:11:51 +0800

    Add maintainWidth option, fix hydration bugs, add more type and compilation safety

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 // types
 export interface ScramblingResult {
@@ -10,21 +10,21 @@ export interface ScramblingResult {
  * Scrambles a text string by incrementally progressing through a collection of frames.
  * @param {string} text - The text to scramble.
  * @param {boolean} hasPlayed - Mutable flag for playing the scrambled text effect.
- * @param {boolean} duration - How many miliseconds each frame takes.
+ * @param {number} duration - How many milliseconds each frame takes.
+ * @param {boolean | undefined} maintainWidth - Whether the text should grow in width as it's scrambling.
  */
 const useScramble = (
   text: string,
   hasPlayed: boolean,
-  duration: number | undefined = 30
+  duration?: number,
+  maintainWidth: boolean | undefined = true,
 ): ScramblingResult => {
   // Shuffle the characters from the text string
   const shuffle = (): string =>
     text
       .split("")
       // randomizes the word
-      .sort(() => {
-        return 0.5 - Math.random();
-      })
+      .sort(() => 0.5 - Math.random())
       // reassembles the word
       .join("");
 
@@ -43,20 +43,29 @@ const useScramble = (
    */
   const generateTextFrames = () => {
     // collection of frames
-    let scrambledTextFrames: string[] = [];
+    const scrambledTextFrames: string[] = [];
 
-    // if text to scramble is not empty
+    // check if text to scramble is not empty
     if (text) {
       // generate the scrambled text frames for each letter of the string
       for (let x = text.length; x >= 0; x--) {
-        // generate a string with shuffled characters
         let tmp = shuffle();
 
-        // cut scrambled text string to the index position of the latest letter
-        tmp = tmp.slice(0, text.length - x);
+        // generate scrambled text frame
+        if (maintainWidth) {
+          // get the current index position
+          const current: number = text.length - x;
 
-        /** push to scrambledTextFrames array to
-         * populate the collection of frames */
+          /** concatenate unscrambled text with scrambled text
+           * up until the current index position */
+          tmp = `${text.slice(0, current)}${tmp.slice(current)}`;
+        } else {
+          /** cut scrambled text string to
+           * the index position of the latest letter */
+          tmp = tmp.slice(0, text.length - x);
+        }
+
+        // add scambled text frame to the collection
         scrambledTextFrames.push(tmp);
       }
 
@@ -68,44 +77,68 @@ const useScramble = (
   };
 
   // collection of scrambled text frames to progress through
-  const [scrambledTextFrames] = useState<string[]>(generateTextFrames);
+  const [scrambledTextFrames, setScrambledTextFrames] = useState<string[]>([""]);
 
-  // positional counter for displaying the current scrambled text frame
+  // tracks which frame of the scramble animation is currently displayed
   const [latestFrame, setLatestFrame] = useState(0);
 
-  // flag for tracking if the animation has finished
+  // indicates whether the animation has completed at least once
   const [hasAnimated, setHasAnimated] = useState<boolean>(false);
+
+  // watcher that shuffles
+  useEffect(() => {
+    // check if animation has finished playing
+    if (!hasPlayed) return;
+
+    // scramble text
+    setScrambledTextFrames(generateTextFrames());
+
+    /** resets latest frame for
+     * when the animation isn't played a single time
+     *
+     * setLatestFrame(0); */
+
+    /** reset has animated flag for
+     * when the animation isn't played a single time
+     *
+     * setHasAnimated(false); */
+  }, [hasPlayed, text, maintainWidth, generateTextFrames]);
 
   // recursive watcher that triggers when the latestFrame index changes
   useEffect(() => {
-    // recursive timer
-    let interval: NodeJS.Timeout | null = null;
+    // check if animation has finished playing
+    if (!hasPlayed) return;
+
+    // stop animation when all the text frames have been iterated through
+    if (latestFrame >= scrambledTextFrames.length - 1) {
+      setHasAnimated(true);
+      return;
+    }
 
     /** triggers when latestFrame changes
     /* checks if:
     /* - the effect is playing
     /* - the position of the latestFrame is
     /*     not equal to the last index of the word */
-    if (hasPlayed && latestFrame < scrambledTextFrames.length - 1) {
-      interval = setInterval(
-        () => {
-          setLatestFrame(latestFrame + 1);
-        },
-        duration ?? 30 // scramble text duration
-      );
-    }
-
-    // set flag to conclude animation
-    if (latestFrame === scrambledTextFrames.length - 1) setHasAnimated(true);
+    const interval = setTimeout(
+      () => {
+        setLatestFrame((f) => ++f);
+      },
+      duration ?? 30,
+    );
 
     // cleanup function on unmount
-    return () => {
-      if (interval) clearInterval(interval);
-    };
-  }, [hasPlayed, latestFrame]);
+    return () => clearTimeout(interval);
+  }, [hasPlayed, latestFrame, duration, scrambledTextFrames.length]);
+
+  // prevent out of bounds index access
+  const latest: number =
+    Math.min(latestFrame, scrambledTextFrames.length - 1) ?? 0;
 
   return {
-    text: scrambledTextFrames[latestFrame],
+    text:
+      scrambledTextFrames[latest] ?? // coalesce to text prop for safety on hydration
+      text,
     hasAnimated,
   };
 };
